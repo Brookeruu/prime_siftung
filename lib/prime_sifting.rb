ab0b635f65f6700fd4fa50b535d51f3f5a68f79f
brooke perry and Daniel Bennett <brooke@>
2018-11-15 11:34:20 -0800

    simplify array construction

--- lib/prime_sifting.rb
+++ lib/prime_sifting.rb
@@ -1,9 +1,16 @@
 def prime_sifting(number)
-  x = 0
-  array = []
-  until (x >= number)
-    x = x + 1
-    array.push(x)
-  end
-  array
+  array = *(2..number)
+  array.delete_if {|a| a.even?}
+  array.delete_if {|a| a %3 == 0}
+  array.delete_if {|a| a %5 == 0}
+  array.delete_if {|a| a %7 == 0}
+  array.insert(0, 2, 3, 5, 7)
 end
+
+## the long way of making an array
+# x = 0
+# array = []
+# until (x >= number)
+#   x = x + 1
+#   array.push(x)
+# end
--- spec/prime_sifting_spec.rb
+++ spec/prime_sifting_spec.rb
@@ -3,7 +3,7 @@ require 'pry'
 require 'prime_sifting'
 
 describe('#prime_sifting') do
-  it('takes a number and makes an array') do
-    expect(prime_sifting(5)).to(eq([1, 2, 3, 4, 5]))
+  it('takes a number and makes an array of prime numbers') do
+    expect(prime_sifting(25)).to(eq([2, 3, 5, 7, 11, 13, 17, 19, 23]))
   end
 end
